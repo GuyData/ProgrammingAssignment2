45a48b83bdf20ff6034104ee7ca3a0639f49d18f
GuyData <GuyData@users.noreply.github.com>
2015-03-23 00:54:00 +1100

    Signed-off-by: GuyData <GuyData@users.noreply.github.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of a matrix
+## Author: Guy Jason Shaw
+## Date: 22/03/2015
 
-## Write a short comment describing this function
+## Student assignment for the Coursera course 'R Programming'
+## by Roger D. Peng, PhD, Jeff Leek, PhD, Brian Caffo, PhD
+## Programming Assignment 2: Lexical Scoping
+
+## This R Script file includes the functions: makeCacheMatrix, cacheSolve
+
+
+## makeCacheMatrix takes a matrix object as input. It creates a special "matrix"
+## object as output that can cache both the input matrix and it's inverse and
+## allow both to be returned or replaced using it's get, set, getinv and setinv
+## functions.
 
 makeCacheMatrix <- function(x = matrix()) {
+    ## If no input matrix is provided then default to an empty matrix.
 
+    ## Initialize the inverted matrix cache.
+    inv <- NULL
+
+    ## Function to allow input matrix to be changed.
+    ## It will will also clear the inverted matrix cache.
+    set <- function(x.new = matrix()) {
+        x <<- x.new
+        inv <<- NULL
+    }
+
+    ## Function to return the cached input matrix
+    get <- function() x
+
+    ## Function to cache the inverted matrix
+    setinv <- function(inverted) inv <<- inverted
+
+    ## Function to return the cached inverted matrix
+    getinv <- function() inv
+
+    ## Return the four functions as a list
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes an object returned by makeCacheMatrix above plus any
+## additional arguments to be passed to the solve function. It will return the
+## cached value of the inverted matrix or use the solve function to calculate it
+## This function assumes that the matrix supplied is always invertible.
+## Since this function is specifically to calculate the inverse of a matrix,
+## the b argument of solve is intentionally missing so it will default to an
+## identity matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Check if the inverse has already been calculated and if so return it
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached inverse")
+        return(inv)
+    }
+
+    ## Otherwise calculate the inverse of the matrix, cache it and return it
+    data <- x$get()
+    message("calculating new inverse")
+    inv <- solve(a=data,b=, ...)
+    x$setinv(inv)
+    inv
 }
